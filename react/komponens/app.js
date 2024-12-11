1add305ce165049c4ca59f99909f6ad2216914a2
rozlev <rozlev404@hengersor.hu>
2024-12-11 13:09:09 +0100

    2024_12_11_comment
    commentekkel egy doboz

--- react/komponens/app.js
+++ react/komponens/app.js
@@ -1,36 +1,33 @@
-function App(){
+function App() {
+    // Az App komponens egy "div"-et hoz létre, amely tartalmazza a "Doboz" szöveget 
+    // és egy másik React komponens, a BoxComponent példányát.
     return React.createElement(
         "div",
         {
-            className:"border"
+            className: "border" // A "div" HTML elemhez egy "border" osztály van hozzárendelve.
         },
         "Doboz", 
-        React.createElement(BoxComponent)
- 
- 
+        React.createElement(BoxComponent)// Itt hívjuk meg a BoxComponent komponenst, amely egy piros dobozt jelenít meg.
     );
 }
- 
- 
-function BoxComponent(props){
+
+function BoxComponent(props) {
+    // A BoxComponent egy piros dobozt hoz létre, meghatározott stílusokkal.
     return React.createElement(
         "div",
- 
         {
- 
-            style:{
-                width:"200px",
-                height:"200px",
-                backgroundColor:"red",
- 
+            style: {
+                width: "200px", 
+                height: "200px", 
+                backgroundColor: "red", 
             },
-            className:"p-2 m-5 rounded",
+            className: "p-2 m-5 rounded", // Bootstrap osztályok: padding, margin, és lekerekített sarkok.
         },
     );
 }
-ReactDOM.render (
-    React.createElement(App),
-    document.getElementById("app-container")
- 
+
+// Az alkalmazást a ReactDOM.render függvény rendereli az app-container azonosítójú DOM elembe.
+ReactDOM.render(
+    React.createElement(App), // Itt hívjuk meg az App komponenst, amely a teljes alkalmazást tartalmazza.
+    document.getElementById("app-container") // Az "app-container" HTML elemhez csatoljuk a React alkalmazást.
 );
- 
