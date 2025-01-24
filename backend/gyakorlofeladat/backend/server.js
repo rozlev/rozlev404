766f7ad0095b187ba4c612006d6952d0857649b5
rozlev <rozlev404@hengersor.hu>
2025-01-24 13:20:51 +0100

    server.js
    server.js fejlesztése

--- backend/gyakorlofeladat/backend/server.js
+++ backend/gyakorlofeladat/backend/server.js
@@ -1,20 +1,31 @@
 const express = require("express");
-const app = require();
+const app = express();
 const cors = require("cors");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
-app.request(bodyParser.json());
-app.use(cors());
-
-const db=mysql.createConnection(
-    {
-        user:"root",
-        host:"127.0.0.1",
-        password:"",
-        database:"kozutak",
+app .use(bodyParser.json());
+ 
+const db = mysql.createConnection(
+    {       
+     user:"root",
+    host:"127.0.0.1",
+    password:"",
+    database:"kozutak",
     }
 );
-
-app.get("/",(req, res) =>{
-    res.send("Fut a backend")
+ 
+app.get("/", (req, res) => {
+    res.send("Fut a backend!");
 })
+ 
+app.get("/regiok",(req, res) => {
+    const sql = "SELECT * FROM `regiok`";
+    db.query(sql, (err, result) => {
+        if(err) return res.json(err);
+        return res.json(result)
+    })
+})
+ 
+app.listen(3001, () => {
+    console.log("Server is runningg on port 3001");
+});
